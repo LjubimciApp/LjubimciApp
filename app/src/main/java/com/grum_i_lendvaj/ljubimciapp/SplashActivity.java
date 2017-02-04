4bffc1095a8e021b65f9c894c64f88cbdad1dfca
Log000 <lovro.grum@gmail.com>
2017-02-04 18:00:45 +0100

    niš bitno

--- app/src/main/java/com/grum_i_lendvaj/ljubimciapp/SplashActivity.java
+++ app/src/main/java/com/grum_i_lendvaj/ljubimciapp/SplashActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_TIME_OUT = 5000;
+    private static final long SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
